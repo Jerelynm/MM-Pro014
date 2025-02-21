53c6110bbea745c91b0fd61111ea05efe3921865
Jerelynm <jmarinm4@miumg.edu.gt>
2025-02-21 16:21:47 -0600

    Numeros del 1 al 20 forma ascendente

---
+++ Asc.cpp
@@ -0,0 +1,15 @@
+// BUCLES WHILE
+// Los numeros del 1 al 20 de forma ascendente
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i=1;
+    while (i<=20) {
+        cout << i << " ";
+        i++;
+    }
+    cout<<endl;
+    return 0;
+}
